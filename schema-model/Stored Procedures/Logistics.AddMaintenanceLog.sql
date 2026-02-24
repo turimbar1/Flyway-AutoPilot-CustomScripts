5272ab0c66f08ce7dfaef2761304d56862877ccf
turimbar1 <andrew.craig.pierce@gmail.com>
2026-02-24 14:10:29 -0800

    go

--- schema-model/Stored Procedures/Logistics.AddMaintenanceLog.sql
+++ schema-model/Stored Procedures/Logistics.AddMaintenanceLog.sql
@@ -2,9 +2,9 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
 CREATE PROCEDURE [Logistics].[AddMaintenanceLog] @FlightID INT, @Description NVARCHAR(500)
 AS BEGIN
+-- Dostuff
     INSERT INTO Logistics.MaintenanceLog(FlightID, Description, MaintenanceStatus)
     VALUES(@FlightID, @Description, 'Pending');
     PRINT 'Maintenance log entry created.';
